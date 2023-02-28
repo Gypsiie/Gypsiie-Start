2548393ecdbd012ad4087443b0c36b66e428ae8e
Kachi Ogbodo <kachie.ogbodo@gmail.com>
2023-02-28 04:34:33 +0000

    Vehicle Class

--- src/test/java/QuestionsTest.java
+++ src/test/java/QuestionsTest.java
@@ -8,14 +8,14 @@ public class QuestionsTest {
     public void testQuestion1() {
         int expectedResult = (int) Math.PI / 1;
         int actualResult = q.question1();
-        Assert.assertEquals(expectedResult, actualResult);
+        Assert.assertEquals(3, 2);
     }
 
     @Test
     public void testQuestion2() {
         int expectedResult = (int) Math.pow(2, 4) - 2;
         int actualResult = q.question2();
-        Assert.assertEquals(expectedResult, actualResult);
+        Assert.assertEquals(14, 5);
     }
 
     @Test
